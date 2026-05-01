544474232aa09174a423c27d4b3f23cb8f308737
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-05-01 16:24:14 -0600

    add CORS as middleware

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from routers import composers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  #allow cookies
+    allow_methods=["*"],
+    allow_headers=["*"],     #allow header (token)
+)
+
 
 app.include_router(composers.router)
--- routers/composers.py
+++ routers/composers.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/api/composers", tags=["composers"])
 
 @router.get("/categories")
 async def get_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
-    composers_list = await composers.get_composers(db ,skip,limit)
+    composers_list = await composers.get_composers(db, skip, limit)
 
     return {
         "code": 200,
